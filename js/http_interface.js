f8ad9721106b6a530e76f7f5028aa815366b7c2f
wujun <junwu1@kaifa.cn>
2018-05-28 16:42:21 +0800

    完成添加删除参数功能-基本功能完成版本

--- js/http_interface.js
+++ js/http_interface.js
@@ -9,7 +9,7 @@ let requestObj = {
 $(document).ready(function(){
   //提交按钮触发
   $('#submit').click(function(){
-    console.log('ok');
+    // console.log('ok');
     setSend();
   });
 })
@@ -25,7 +25,14 @@ function getTableValue(){
     let value = $('[name = values]')[i].value;
     requestObj.parameter[key] = value;
 
-    console.log(key, value);
+    // console.log(key, value);
+  }
+}
+
+function getCheckedStatus(){
+  //不发送参数
+  if($('#openParameter').prop('checked') === false){
+    requestObj.parameter = {};
   }
 }
 
@@ -33,12 +40,13 @@ function getTableValue(){
 function setSend(){
   let url = $('#url').val(); //获取输入框url
   let httpType = $('#type').val(); //获取请求类型
-  getTableValue();
+  getTableValue();//获取参数数据
+  getCheckedStatus();//判断是否发送数据
 
   requestObj.url = url;
   requestObj.type = httpType;
 
-  sendRequest();
+  sendRequest();//发送请求
 }
 
 //发送请求
@@ -49,17 +57,42 @@ function sendRequest(){
 
   if(type === 'Get'){
     $.get(url, parameter, function(res){
-      console.log(res);
+      consoleAction(res);
       setJsonHtml(res);
     })
   }else if(type === 'Post'){
     $.post(url, parameter, function(res){
-      console.log(res);
+      consoleAction(res);
       setJsonHtml(res);
     })
   }
 }
 
+function consoleAction(res){
+  try{
+    console.log(JSON.parse(res));
+  }catch(e){
+    console.log(res);
+  }
+}
+
 function setJsonHtml(row){
   $('#json_input').html(row);
+}
+
+//添加参数事件
+function add_url_parameter(that){
+  let element = that.parentNode.parentNode;
+  // console.log(element);
+  let Trhtml = '<tr class="key">' + 
+  '<td><input k="" value="" name="keys" class="form-control" type="text" maxlength="100" placeholder="参数名称"></td> ' +
+  '<td><input value="" v="" name="values" class="form-control" type="text" maxlength="5000" style="width: 70%;float: left;" placeholder="参数数值"><button type="button" onclick="del(this);" class="btn btn-primary btn-sm del_btn">删除参数</button></td>' +
+  '</tr>'
+  $(element).before(Trhtml);
+}
+
+function del(that){
+  let element = that.parentNode.parentNode;
+  // console.log(element);
+  $(element).remove();
 }
